fdf52bd5f84b929ee769526754ae17e4c192921c
ztc1997 <dev@ztc1997.com>
2025-06-16 08:58:26 +0000

    kernel: cpuset: Add more services to tampering blacklist
    https://github.com/hfdem/android_gki_kernel_5.15_common/commit/bf732f37299c26fef2ec61673f5e27b101d32ba2
    https://github.com/hfdem/android_gki_kernel_5.15_common/commit/5a87af26014c1831c597df75f0358fd4fdfaaa4b
    https://github.com/hfdem/android_gki_kernel_5.15_common/commit/6b7185ed2313552c7ff78ea0008b647ac81264d3
    
    Signed-off-by: bintang774 <108184157+bintang774@users.noreply.github.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -219,7 +219,11 @@ bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@2.2-servic") ||
 	       !strcmp(comm, "power@2.0-servi") ||
 	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	       !strcmp(comm, "PERFD-SERVER") ||
+	       !strcmp(comm, "init.qcom.post_") ||
+	       !strcmp(comm, "mtkPowerMsgHdl") ||
+	       !strcmp(comm, "mtkPowerService") ||
+	       !strncmp(comm, "system_perf_ini", 9);
 }
 
 static inline struct cpuset *css_cs(struct cgroup_subsys_state *css)
