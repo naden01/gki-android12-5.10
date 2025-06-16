8dc905d80340190516feb6d9ea0b28fb0d24b97f
bintang774 <108184157+bintang774@users.noreply.github.com>
2025-06-16 08:25:48 +0000

    zram: Forbid init to change zram comp
    Inspirated from: https://github.com/EmanuelCN/kernel_xiaomi_sm8250/commit/d5ab8e8fa9e7f8f085074af3a5d91d0fb62ace92
    
    Signed-off-by: bintang774 <108184157+bintang774@users.noreply.github.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1014,6 +1014,8 @@ static ssize_t comp_algorithm_show(struct device *dev,
 	return sz;
 }
 
+bool task_is_booster(struct task_struct *tsk);
+
 static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
@@ -1026,6 +1028,9 @@ static ssize_t comp_algorithm_store(struct device *dev,
 	sz = strlen(compressor);
 	if (sz > 0 && compressor[sz - 1] == '\n')
 		compressor[sz - 1] = 0x00;
+
+	if (task_is_booster(current))
+		return -EPERM;
 
 	if (!zcomp_available_algorithm(compressor))
 		return -EINVAL;
